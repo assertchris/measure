1f6fdab02eab36736e3c753f94f1a945855a5d0b
Christopher Pitt <cgpitt@gmail.com>
2020-04-07 19:41:32 +0200

    Update question copy

--- resources/lang/en/questions.php
+++ resources/lang/en/questions.php
@@ -1,64 +1,24 @@
 <?php
 
 return [
-    'quality_of_work_title' => 'How is your dev?',
-    'quality_of_work_description' => '
-- Are you proficient enough?
-- Are you delivering good work, in reasonable time?
-- Do you feel good about it?
-    ',
-    'quality_of_handover_title' => 'How are your testing and documentation?',
-    'quality_of_handover_description' => '
-- Do you know how to test and document the projects you&apos;re working on?
-- Are you thorough?
-- Have you been handing over work in the best way?
-    ',
+    'quality_of_work_title' => 'How is your work?',
+    'quality_of_work_description' => 'Do you feel like you know enough to do your work, or are there areas you&apos;d like to improve your skills? Are you proud of your work, and are you delivering it in a reasonable amount of time?',
+    'quality_of_handover_title' => 'How are you handing over work to other people?',
+    'quality_of_handover_description' => 'Do you know the proper way to hand over the projects and tasks you are working on? Are you giving enough information for how to take the work over, and is it written down somewhere?',
     'communicating_well_title' => 'How is your communication?',
-    'communicating_well_description' => '
-- Have you been attending every meeting, prepared and on-time?
-- Are you proactive; communicating when appropriate, not just when asked for an update?
-- Have you been recording important details, like notes and questions, in places like Asana and Slack?
-- Have you been keeping up to date with your Asana cards?
-    ',
+    'communicating_well_description' => 'Have you been attending meetings prepared and on time? Are you proactive in communicating when tasks will be done, or when they can&apos;t be done? Are you keeping up to date with your Asana cards?',
     'being_a_good_listener_title' => 'Have you been listening?',
-    'being_a_good_listener_description' => '
-- Are you focussed on improving, based on recent feedback?
-- Are you listening more than responding?
-    ',
+    'being_a_good_listener_description' => 'Are you seeking out feedback, and when you get it are taking the time to figure out what parts of it to apply? Are you mindfully listening?',
     'following_though_title' => 'Are you sticking to your word?',
-    'following_though_description' => '
-- Are you considering, carefully, what you agree to?
-- When you say you&apos;re going to do something, are you doing your best to get it done in that time?
-- Are you communicating early and clearly when you realise you won&apos;t make a deadline?
-    ',
+    'following_though_description' => 'Are you carefully saying yes and no? When you say you&apos;re going to do something, are you doing your best to see that it gets done on time? Are you communicating early and clearly when you don&apos;t think something will be done on time?',
     'enjoying_indie_title' => 'Are you enjoying being here?',
-    'enjoying_indie_description' => '
-- Do you enjoy what you&apos;re working on?
-- Do you enjoy the people you&apos;re working with?
-    ',
+    'enjoying_indie_description' => 'Do you enjoy the stuff you work on, in general? How about the people you work with? Do you feel like you could be honest about moving to a role you&apos;d prefer?',
     'overseeing_work_title' => 'Are you overseeing work well?',
-    'overseeing_work_description' => '
-- Are you mindful of the working wellbeing of the people reporting to you?
-- Are you clear in your instructions and expectations?
-- Are you seeking ways to be a better manager and team?
-    ',
+    'overseeing_work_description' => 'Are you mindful of the wellbeing of people who are reporting to you? Are you clear about what you expect of them, and how you feel about the work they do? Are you looking for ways to better manage this kind of work?',
     'being_a_mentor_title' => 'Are you being a good mentor?',
-    'being_a_mentor_description' => '
-- Are you making enough time to give the necessary attention to your mentees?
-- Are you being kind and avuncular to your mentees?
-    ',
+    'being_a_mentor_description' => 'Are you making enough time to give good advice, or as good questions, of the people who you are mentoring? Are you being avuncular to the people who you mentor?',
     'engaging_in_learning_title' => 'Are you engaging in self-directed learning?',
-    'engaging_in_learning_description' => '
-- Are you being responsible for improving in the skills of your domain?
-- Are you being clear about your needs, to the people you report to?
-- Have you documented your career development path?
-- Do you make time to regularly review your career development path?
-    ',
+    'engaging_in_learning_description' => 'Are you actively looking for ways to improve your skills? This can be done during normal "office hours", but it takes clear planning and communication. Are you being clear about what the company can do to help you improve. Have you documented your career growth path, and do you regularly take time to review your goals?',
     'sharing_knowledge_title' => 'Are you sharing your knowledge and skills?',
-    'sharing_knowledge_description' => '
-- Can you explain what you do here?
-- Have you written that down, in case you suddenly and unexpectedly take leave?
-- Are you seeking ways to tell people outside of the company what we do?
-- Are you seeking feedback from industry experts?
-    ',
+    'sharing_knowledge_description' => 'Can you explain what you do at the company, and have you written down the hard parts so someone can step in if you need to take time off? Are you seeking knowledge and/or feedback and/or mentoring from industry experts, outside of the company?',
 ];
